eeb562c713435df6511bfe106ac0fc508e5d3bc8
Fuat Yöndem <56515802+Fuat43@users.noreply.github.com>
2021-04-11 19:37:18 +0200

    remove conn data

--- www/php/dbcon.php
+++ www/php/dbcon.php
@@ -1,9 +1,9 @@
 <?php
     // vervang deze gegevens door de gegevens van jouw server!
-    $servername = "ID329262_bubu.db.webhosting.be";
-    $username = "ID329262_bubu"; // username (zie Hosting)
-    $password = "Zorbulunur56"; // paswoord DATABANK (zie hosting)
-    $dbname = "ID329262_bubu"; // naam databank (zie hosting ; zelf gekozen)
+    $servername = "";
+    $username = ""; // username (zie Hosting)
+    $password = ""; // paswoord DATABANK (zie hosting)
+    $dbname = ""; // naam databank (zie hosting ; zelf gekozen)
         
     // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die(mysqli_connect_error());
