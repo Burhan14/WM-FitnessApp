820a082102dd16bd5d989424763822aa86fc3692
Fuat Yöndem <fuat.yondem@student.odisee.be>
2021-06-06 16:59:23 +0200

    .

--- www/js/app.js
+++ www/js/app.js
@@ -19,7 +19,6 @@ var bmi;
 var exercisesList = [];
 var sessionVirtualList;
 var sessionItems = [];
-
 var user;
 var fs;
 //#endregion
@@ -570,22 +569,6 @@ var app = new Framework7({
     },
   },
 });
-
-//#region LOGIN (TO IMPLEMENT)
-
-// Login Screen Demo 
-$$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
-
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
-});
-
-//#endregion
 
 //#region hulpmethodes
 
